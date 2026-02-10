964f40b9132863b5e9296d7110bdb481048f4d8d
alxmra <miguelamneto@ua.pt>
2026-02-10 01:41:46 +0000

    fix

--- src/models/db.py
+++ src/models/db.py
@@ -2,14 +2,14 @@ from sqlmodel import Field, SQLModel
 
 
 class Decision(SQLModel, table=True):
-    __tablename__: str = "decisions"
+    __tablename__ = "decisions"
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True)
-    description: str | None = None
+    description: str | None = Field(default=None)
 
 
 class Blacklist(SQLModel, table=True):
-    __tablename__: str = "blacklist"
+    __tablename__ = "blacklist"
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True)
-    reason: str | None = None
+    reason: str | None = Field(default=None)
